340903a830952270b8ff9a635d03c9b5f9b97890
Ash Joory <10279095+ashjoory@users.noreply.github.com>
2025-06-04 10:49:48 +0100

    Release v1.0.10

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,6 +13,10 @@ class Plugin implements PluginInterface, EventSubscriberInterface
 {
     public function activate(Composer $composer, IOInterface $io) {}
 
+    public function deactivate(Composer $composer, IOInterface $io) {}
+
+    public function uninstall(Composer $composer, IOInterface $io) {}
+
     public static function getSubscribedEvents(): array
     {
         return [
